2f131c2f11b049725d2a7849405c0cfc78f2b632
Tylerjaydenne Anthony <tylerjaydenne127@gmail.com>
2022-11-14 00:46:47 -0500

    commit tree 34bf9642e2223aad03b345f57d74f4a4395bad68

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b){
+return a + b;
+}
+function subtract(a,b){
+   return a - b;
+}
+function multiply(a,b){
+    return a * b;
+    }
+function divide(a,b){
+    return a / b;
+    }
+    function increment(n){
+    return n + 1;
+    }
+    function decrement(n){
+    return n - 1;
+    }
+function parseInt(n){
+   return makeInt('n');
+   // MakeInt parses n
+}
+function makeInt(n){
+    return parseInt(String, 0);
+}
+function makeInt(n){
+    return ('sldkjflksjf', 0);
+ }    //makeInt return NaN 
+
+function parseFloat(n){
+    return preserveDecimal(2.222);
+}
+function preserveDecimal(n){
+    return ('sldkjflksjf');
+}
